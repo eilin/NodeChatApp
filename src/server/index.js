3567edd16eff93efadbaaa44d0c57e3b7850586d
edward lin <eilin@debianvb.51havemeyer3f>
2021-03-06 17:36:39 -0500

    clean up server code

--- src/server/index.js
+++ src/server/index.js
@@ -7,23 +7,19 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "./../../")));
 
-app.get('/', function(req, res,next) {
+app.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, "./../../index.html"));
 });
 
 io.on('connection', function (socket) {
-  console.log("new client connected" , socket.handshake.address);
+  console.log("new client connected", socket.handshake.address);
   socket.on("send_message", (data) => {
     console.log("Broadcasting message: " + data.message);
     socket.broadcast.emit("receive_message", data);
   });
 });
 
-io.on('message', (socket) => {
-  socket.broadcast.emit('message', {content: "this is a test message"});
-});
-
 server.listen(3000, () => {
-  console.log("Listening on Port 3000");
+  console.log("Listening on port 3000");
 });
 
